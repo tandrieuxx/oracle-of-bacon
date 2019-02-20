43a6371943e49363c06f28db308df8c22b177a03
Unknown <tandrieux@outlook.com>
2019-02-20 16:16:58 +0100

    Fix : débouchonnage de la recherche d'acteurs

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
@@ -6,6 +6,7 @@ import com.serli.oracle.of.bacon.repository.Neo4JRepository;
 import com.serli.oracle.of.bacon.repository.RedisRepository;
 import net.codestory.http.annotations.Get;
 import net.codestory.http.convert.TypeConvert;
+import org.bson.Document;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -56,20 +57,6 @@ public class APIEndPoint {
 
     @Get("actor?name=:actorName")
     public String getActorByName(String actorName) {
-        return "{\n" +
-                "\"_id\": {\n" +
-                "\"$oid\": \"587bd993da2444c943a25161\"\n" +
-                "},\n" +
-                "\"imdb_id\": \"nm0000134\",\n" +
-                "\"name\": \"Robert De Niro\",\n" +
-                "\"birth_date\": \"1943-08-17\",\n" +
-                "\"description\": \"Robert De Niro, thought of as one of the greatest actors of all time, was born in Greenwich Village, Manhattan, New York City, to artists Virginia (Admiral) and Robert De Niro Sr. His paternal grandfather was of Italian descent, and his other ancestry is Irish, German, Dutch, English, and French. He was trained at the Stella Adler Conservatory and...\",\n" +
-                "\"image\": \"https://images-na.ssl-images-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_UY317_CR13,0,214,317_AL_.jpg\",\n" +
-                "\"occupation\": [\n" +
-                "\"actor\",\n" +
-                "\"producer\",\n" +
-                "\"soundtrack\"\n" +
-                "]\n" +
-                "}";
+        return this.mongoDbRepository.getActorByName(actorName).map(Document::toJson).orElse("[]");
     }
 }
